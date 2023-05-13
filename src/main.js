be23d3c33c25dafe03bab8d41869ad7b56cd8e35
pickingname <trintrinir@icloud.com>
2023-05-13 17:26:23 +0700

    percent transition
    controversial change (i dont really like it idk might change back)

--- src/main.js
+++ src/main.js
@@ -5,5 +5,14 @@ window.onload = function() {
   var second = now.getSeconds();
   var totalSeconds = (hour * 60 * 60) + (minute * 60) + second;
   var percent = (totalSeconds / 86400) * 100;
-  document.getElementById("daypercent").innerHTML = percent.toFixed(0) + "% complete";
+  var elem = document.getElementById("daypercent");
+  var count = 1;
+  var interval = setInterval(function() {
+    if (count <= percent) {
+      elem.innerHTML = count + "% complete";
+      count++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 100); // Fixed interval of 0.2 seconds
 }
